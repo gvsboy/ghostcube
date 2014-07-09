02693b8eed14efb476a8278b79512d63460ce7ff
Kevin Mikles <kmikles@linkedin.com>
2014-07-09 00:21:25 -0700

    More style work with mixins and vendor prefixes.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 (function(win) {
 
-  var ROTATE_X_PREFIX = 'rotateX(',
+  var TRANSFORM = 'Transform',
+      ROTATE_X_PREFIX = 'rotateX(',
       ROTATE_Y_PREFIX = 'rotateY(',
       ROTATE_UNIT_SUFFIX = 'deg)',
       REVOLUTION = 360,
@@ -8,16 +9,34 @@
       cube = doc.getElementById('cube'),
       cubeStyle = cube.style,
       tick = 0,
+      transformProperty,
       timer;
+
+  function getVendorPrefix() {
+
+    var style = document.body.style,
+        prefixes = ['ms', 'O', 'Moz', 'Webkit', ''],
+        prefix;
+
+    while (prefixes.length) {
+      prefix = prefixes.pop();
+      if ((prefix + TRANSFORM) in style) {
+        return prefix;
+      }
+    }
+
+    return '';
+  }
 
   function rotate() {
     tick++;
-    cubeStyle.transform = ROTATE_X_PREFIX + tick + ROTATE_UNIT_SUFFIX + ' ' + ROTATE_Y_PREFIX + tick + ROTATE_UNIT_SUFFIX;
+    cubeStyle[transformProperty] = ROTATE_X_PREFIX + tick + ROTATE_UNIT_SUFFIX + ' ' + ROTATE_Y_PREFIX + tick + ROTATE_UNIT_SUFFIX;
     if (tick === REVOLUTION) {
       tick = 0;
     }
   }
 
+  transformProperty = getVendorPrefix() + TRANSFORM;
   timer = win.setInterval(rotate, 10);
 
 }(window));
