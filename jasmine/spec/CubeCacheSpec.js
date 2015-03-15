ebb06a9f27c839b216a7d917267cf0270397cf53
Kevin Mikles <kmikles@linkedin.com>
2015-03-15 09:11:10 -0700

    Improved CubeCacheSpec with helpers

--- jasmine/spec/CubeCacheSpec.js
+++ jasmine/spec/CubeCacheSpec.js
@@ -1,9 +1,52 @@
 describe('CubeCache', function() {
 
+  /**
+   * Add tiles using CubeCache's add method and the global getTiles method.
+   * Claim them first to make add work properly.
+   * @param {...String} Any number of strings representing tiles.
+   */
+  function add() {
+
+    // Arguments to pass to getTiles.
+    var args = [this.cube].concat(_.toArray(arguments)),
+        tiles = getTiles.apply(this, args);
+
+    // For each tile, claim it and then add it to the cache.
+    _.forEach(tiles, function(tile) {
+      tile.claim(this.player);
+      this.cubeCache.add(tile);
+    }, this);
+  };
+
+  /**
+   * Helper function for return the current number of singles.
+   * @return {Number} The number of singles.
+   */
+  function singlesLength() {
+    return this.cubeCache._singles.length;
+  }
+
+  /**
+   * Helper function for returning the current number of lines.
+   * @return {Number} The number of lines.
+   */
+  function linesLength() {
+    return this.cubeCache.getLines().length;
+  }
+
   beforeEach(function() {
+
+    // Create a new CubeCache instance.
     this.cubeCache = new CubeCache(createCube());
+
+    // Reference the cube and player as some tests need it.
     this.cube = this.cubeCache._cube;
     this.player = new Player('Test', 'player1', this.cube);
+
+    // Wish there was a better way to handle this...
+    this.add = _.bind(add, this);
+    this.singlesLength = _.bind(singlesLength, this);
+    this.linesLength = _.bind(linesLength, this);
   });
 
   it('exists', function() {
@@ -34,12 +77,20 @@ describe('CubeCache', function() {
           ooo
      */
     it('passes scenario 1', function() {
-      var tiles = getTiles(this.cube, 'top 3', 'top 5');
-      _.forEach(tiles, function(tile) {
-        tile.claim(this.player);
-        this.cubeCache.add(tile);
-      }, this);
-      expect(this.cubeCache.getLines().length).toEqual(1);
+      this.add('top 3', 'top 5');
+      expect(this.singlesLength())).toEqual(0);
+      expect(this.linesLength()).toEqual(1);
+    });
+
+    /* Scenario 2:
+          ooo
+          xoo
+          oxo
+     */
+    it('passes scenario 2', function() {
+      this.add('top 3', 'top 7');
+      expect(this.singlesLength()).toEqual(2);
+      expect(this.linesLength()).toEqual(0);
     });
 
   });
