4523de94e90cab8b8ce5f61bdc8a33b63d038665
Kevin Mikles <kmikles@linkedin.com>
2015-04-25 23:01:51 -0700

    Implemented IE fallback

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GhostCube
 
 Just having some spectral fun.
+
+Visit [kevinmikles.com](http://kevinmikles.com) to see it in action.
--- dist/dev/main.js
+++ dist/dev/main.js
@@ -3112,17 +3112,23 @@ function listenOnce(target, type, callback) {
 Object.defineProperty(exports, '__esModule', {
   value: true
 });
+/**
+ * This is a messy file containing vendor compatibility support logic.
+ * Mostly, we're normalizing the a few CSS and JS methods and checking 
+ * whether or not the user's browser can handle the modern CSS we're throwing at it.
+ * But yeah, not very pretty stuff below.
+ */
+
 var STYLE = document.body.style,
     TRANSFORM = 'Transform',
 
 // Prefixes used for things like Transform.
 STYLE_PREFIXES = ['ms', 'O', 'Moz', 'Webkit'],
 
-// Animation end events. Not quite perfect as IE10+
-// actually uses 'animation' -> 'MSAnimationEnd'
-// I'll fix this later.
+// Animation end events. Not quite perfect as IE10+ actually uses:
+// 'animation' -> 'MSAnimationEnd'
 // So ridiculous. Can't these be consistent?!
-// ...
+// Whatever. IE doesn't support preserve-3d anyways so no big deal.
 // Map format:
 // 'css-attribute':       [start, iteration, end]
 ANIMATION_EVENT_MAP = {
@@ -3131,10 +3137,7 @@ ANIMATION_EVENT_MAP = {
   '-moz-animation': ['animationstart', 'animationiteration', 'animationend'],
   '-webkit-animation': ['webkitAnimationStart', 'webkitAnimationIteration', 'webkitAnimationEnd']
 },
-    msAnimationEnd = 'MSAnimationEnd',
-    //TODO
-
-len = STYLE_PREFIXES.length,
+    len = STYLE_PREFIXES.length,
     stylePrefix,
     animationProperty,
     eventTypes,
@@ -3174,6 +3177,49 @@ for (animationProperty in ANIMATION_EVENT_MAP) {
 
 // Normalize requestAnimationFrame for cross-browser compatibility.
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+// Check to see if the browser supports preserve-3d. If not, place a
+// special class on the body element.
+(function () {
+
+  var WEBKIT_TRANSFORM_STYLE = '-webkit-transform-style',
+      MOZ_TRANSFORM_STYLE = '-moz-transform-style',
+      MS_TRANSFORM_STYLE = '-ms-transform-style',
+      TRANSFORM_STYLE = 'transform-style',
+      element = document.createElement('p'),
+      body = document.body,
+      properties = {
+    webkitTransformStyle: WEBKIT_TRANSFORM_STYLE,
+    MozTransformStyle: MOZ_TRANSFORM_STYLE,
+    msTransformStyle: MS_TRANSFORM_STYLE,
+    transformStyle: TRANSFORM_STYLE
+  };
+
+  function has3D() {
+
+    var PRESERVE_3D = 'preserve-3d',
+        prop,
+        computedStyle;
+
+    for (prop in properties) {
+      if (element.style[prop] !== undefined) {
+        element.style[prop] = PRESERVE_3D;
+      }
+    }
+
+    computedStyle = window.getComputedStyle(element);
+
+    return (computedStyle.getPropertyValue(WEBKIT_TRANSFORM_STYLE) || computedStyle.getPropertyValue(MOZ_TRANSFORM_STYLE) || computedStyle.getPropertyValue(MS_TRANSFORM_STYLE) || computedStyle.getPropertyValue(TRANSFORM_STYLE)) === PRESERVE_3D;
+  }
+
+  body.appendChild(element);
+
+  if (!has3D()) {
+    body.classList.add('no-3d');
+  }
+
+  body.removeChild(element);
+})();
 
 exports.js = js;
 exports.css = css;
